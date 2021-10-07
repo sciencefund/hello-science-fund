2904b06a0a3d256709b0f0ad88b2c1aea08706ee
Nate Jacobs <natesjacobs@gmail.com>
2021-10-07 09:21:26 -0700

    Update CustomIcons.jsx

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -2,6 +2,6 @@ import makeFAIcon from "utils/makeFAIcon";
 
 import { faShoppingCart, faLaptop, faLock } from "@fortawesome/free-solid-svg-icons";
 
-export const ShoppingCartIcon = makeFAIcon(faShoppingCart);
+export const ShoppingCartIcon = makeFAIcon(faArrowCircleUp);
 export const LaptopIcon = makeFAIcon(faLaptop);
 export const LockIcon = makeFAIcon(faLock);
