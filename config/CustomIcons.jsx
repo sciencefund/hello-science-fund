bdca4311b1237b637f97892ed3772ba8970c1fe4
Nate Jacobs <natesjacobs@gmail.com>
2021-10-08 23:36:58 -0700

    update icons

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -1,9 +1,10 @@
 import makeFAIcon from "utils/makeFAIcon";
 
-import { faAngleDoubleUp, faBurn, faBtc } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleUp, faBurn, faRocket } from "@fortawesome/free-solid-svg-icons";
 
 export const ArrowUpIcon = makeFAIcon(faAngleDoubleUp);
 export const BurnIcon = makeFAIcon(faBurn);
+export const RocketIcon = makeFAIcon(faRocket);
 // export const EthereumIcon = makeFAIcon(faEthereum);
 
 // export const ShoppingCartIcon = makeFAIcon(faArrowCircleUp);
